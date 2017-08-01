8631b2b41e5c5d36751af7c00a90d49aeca1ccc3
Ikram Qabiyev <ikram20011127@gmail.com>
2017-08-01 23:35:23 +0400

    navigation last version

--- js/digital.js
+++ js/digital.js
@@ -14,3 +14,33 @@ $(".navBarChoices ul li a").click(function(elem) {
     $(".navBarChoices ul li a").removeClass("activateLink");
     $(this).addClass("activateLink");
 })
+
+// event listener for services button
+var serCond=false
+var $services = $(".nav .navBar .navBarChoices > ul > li:nth-child(1)");
+var $serDropDown = $(".nav .navBar .navBarChoices > ul > li:nth-child(1) ul")
+$($services).click(function(){
+    if (serCond==false) {
+        $($serDropDown).slideDown()
+        serCond=true;
+    }
+    else {
+        $($serDropDown).slideUp()
+        serCond=false;
+    }
+})
+
+// event listener for services button in the main dropdown
+var mainSerCond=false
+var $mainServices = $(".mainDrop > ul > li:nth-child(1)");
+var $mainSerDropDown = $(".mainDrop > ul > li:nth-child(1) > ul")
+$($mainServices).click(function(){
+    if (mainSerCond==false) {
+        $($mainSerDropDown).slideDown()
+        mainSerCond=true;
+    }
+    else {
+        $($mainSerDropDown).slideUp()
+        mainSerCond=false;
+    }
+})
